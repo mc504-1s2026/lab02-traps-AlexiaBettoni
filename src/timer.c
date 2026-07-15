7e3fd45bc79c18092faa53004596c669cb633a19
Alexia Bettoni do Nascimento <alexiabettoni@yahoo.com.br>
2026-07-15 14:40:30 -0300

    wip: código pré teste

--- src/timer.c
+++ src/timer.c
@@ -1,32 +1,38 @@
 #include <arch/timer.h>
 #include <kernel/panic.h>
+#include <types.h>
+
+#define TIMER_FREQ 10000000
+#define SIE_STIE (1 << 5)
 
 u64 timer_read()
 {
-	/* not implemented */
-	BUG();
+	u64 time;
+    asm volatile("csrr %0, time" : "=r"(time));
+    return time;
 }
 
 void timer_irq_enable()
 {
-	/* not implemented */
-	BUG();
+	u64 mask = SIE_STIE;
+    asm volatile("csrs sie, %0" :: "r"(mask));
 }
 
 void timer_irq_disable()
 {
-	/* not implemented */
-	BUG();
+	u64 mask = SIE_STIE;
+    asm volatile("csrc sie, %0" :: "r"(mask));
 }
 
 void timer_set_alarm(u64 secs)
 {
-	/* not implemented */
-	BUG();
+	u64 now = timer_read();
+    u64 alarm_time = now + (secs * TIMER_FREQ);
+    asm volatile("csrw stimecmp, %0" :: "r"(alarm_time));
 }
 
 void timer_irq()
 {
-	/* not implemented */
-	BUG();
+	u64 disable_val = -1ULL;
+    asm volatile("csrw stimecmp, %0" :: "r"(disable_val));
 }
--- src/trap.c
+++ src/trap.c
@@ -1,53 +1,96 @@
 #include <kernel/trap.h>
 #include <kernel/panic.h>
+#include <types.h>
 
 /* defined in src/trap_entry.S */
 extern void trap_entry();
 
+#define SIE_SEIE (1 << 9)
+
 void handle_irq()
 {
-	/* not implemented */
-	BUG();
+	u64 scause;
+	asm volatile("csrr %0, scause" : "=r"(scause));
+
+	u64 irq_code = scause & ~(1ULL << 63);
+
+	switch (irq_code)
+	{
+	case 5:
+		timer_irq();
+		break;
+	case 9:
+		u32 irq = plic_hart_claim_irq(0);
+
+		if (irq == 10){ 
+			serial_irq();
+		}
+
+		if (irq != 0){
+			plic_hart_complete_irq(0, irq);
+		}
+		break;
+	default:
+		panic("Problema não tratado: %d\n", irq_code);
+	}
 }
 
 void handle_exception()
 {
-	/* not implemented */
-	BUG();
+	u64 scause;
+	u64 stval;
+	asm volatile("csrr %0, scause" : "=r"(scause));
+	asm volatile("csrr %0, stval" : "=r"(stval));
+	if (scause == 12 || scause == 13 || scause == 15)
+	{
+		panic("Page fault: %d", scause);
+	}
+	panic("Problema não tratado: %d", scause)
 }
 
 void trap_setup()
 {
-	/* not implemented */
-	BUG();
+	u64 entry = (u64)trap_entry;
+	asm volatile("csrw stvec, %0" ::"r"(entry));
+
+	u64 mask = SIE_SEIE;
+    asm volatile("csrs sie, %0" :: "r"(mask));
 }
 
 void handle_trap()
 {
-	/* not implemented */
-	BUG();
+	u64 scause;
+	asm volatile("csrr %0, scause" : "=r"(scause));
+
+	if (scause & (1ULL << 63)) {
+		handle_irq();
+	} else {
+		handle_exception();
+	}
 }
 
 void hart_irq_enable()
 {
-	/* not implemented */
-	BUG();
+	u64 mask = 1 << 1;
+	asm volatile("csrs sstatus, %0" ::"r"(mask));
 }
 
 u64 hart_irq_save()
 {
-	/* not implemented */
-	BUG();
+	u64 sstatus;
+	u64 mask = 1 << 1;
+	asm volatile("csrr %0, sstatus" : "=r"(sstatus));
+	asm volatile("csrc sstatus, %0" ::"r"(mask));
+	return sstatus;
 }
 
 void hart_irq_restore(u64 flags)
 {
-	/* not implemented */
-	BUG();
+	asm volatile("csrw sstatus, %0" ::"r"(flags));
 }
 
 void hart_irq_disable()
 {
-	/* not implemented */
-	BUG();
+	u64 mask = 1 << 1;
+	asm volatile("csrc sstatus, %0" ::"r"(mask));
 }
